60ca27ea6e25df11b2c7b0a697ef61cdc97e437e
Pin-Ching Chen <p.ching0811@gmail.com>
2022-06-15 20:27:19 +0800

    edit: config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,17 +7,17 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  tagline: '',
+  url: 'https://cpching.github.io/pching',
+  baseUrl: '/pching/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'cpching', // Usually your GitHub org/user name.
+  projectName: 'pching', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
